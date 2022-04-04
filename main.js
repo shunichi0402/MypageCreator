f0a2e724f21986fdefbb92cf77e9adfd486599d8
shunichi0402 <49316097+shunichi0402@users.noreply.github.com>
2022-04-04 17:28:09 +0900

    add sertserver program

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 const server = require('./library/server');
+const consola = require
+
+server.start()
+    .then( port => consola.success(`Server listen at port${port}...`))
+    .catch(err => consola.error(err));
