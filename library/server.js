cc306418de11a23cd22862358c6661d9773f8552
Shunichi0402 <shunichi_nikoniko@yahoo.co.jp>
2022-04-08 22:13:46 +0900

    add rooting

--- library/server.js
+++ library/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const configs = require('../configs/server_config');
-
+const path = require('path');
+const mainPath = path.dirname(__dirname);
 
 class MainServer{
     constructor(){
@@ -8,10 +9,24 @@ class MainServer{
         this.http = require('http').Server(this.app);
         this.port = configs.port;
         this.serverInit();
+        this.rooting();
     }
 
     serverInit(){
         this.app.set("view engine", "ejs");
+    }
+
+    rooting(){
+        // === TODO ===
+        // Add randering pages rooting
+
+        // static asset
+        this.app.use(express.static(path.join(mainPath, 'static')));
+
+        // 404 page
+        this.app.use((res, req) => {
+            res.status(404).render(path.join(mainPath, 'page', 'notfound'), {})
+        });
     }
 
     async start(){
